402da354e1ccd7d8d1617374a3e12a66b62da0a5
alanams97 <alan_ams97@gmail.com>
2020-02-27 20:59:23 -0300

    Adicionada array 'donors'

--- server.js
+++ server.js
@@ -10,11 +10,34 @@ nunjucks.configure("./", {
 
 // configurando a apresentação da página
 server.get("/", function(req, res){
-    return res.render("index.html")
+    return res.render("index.html", {donors})
 })
 
 // configurar o servidor para apresentar arquivos estáticos 
 server.use(express.static('public'))
+
+
+// Lista de doadores: Array
+const donors = [
+    {
+        name: "Alan Alves",
+        blood: "A+",
+    },
+    {
+        name: "Gustavo Souza",
+        blood: "B+",
+    },
+    {
+        name: "Lucas Silva",
+        blood: "AB+",
+    },
+    {
+        name: "José Airton",
+        blood: "O+",
+    },
+
+]
+
 
 // ouvir na porta 3000
 server.listen(3000, function() {
