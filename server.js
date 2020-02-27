967d53506e91d0d635df381d042aebf901975021
alanams97 <alan_ams97@gmail.com>
2020-02-27 20:49:45 -0300

    Html iniciado

--- server.js
+++ server.js
@@ -1,10 +1,22 @@
+// configurando o servidor
 const express = require("express")
 const server = express()
 
-server.get("/", function(req, res){
-    return res.send("Ok, cheguei aqui com nodemon")
+// configurando o template engine
+const nunjucks = require("nunjucks")
+nunjucks.configure("./", {
+    express: server
 })
 
+// configurando a apresentação da página
+server.get("/", function(req, res){
+    return res.render("index.html")
+})
+
+// configurar o servidor para apresentar arquivos estáticos 
+
+
+// ouvir na porta 3000
 server.listen(3000, function() {
     console.log("Servidor iniciado")
 })
